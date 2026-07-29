8151968491e7e297af8dd2777d515b537202ce49
Rahul Kumar <krahul89kumar@gmail.com>
2026-07-30 01:32:22 +0530

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/bestie-card/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
